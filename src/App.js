bad82f0301de13b9bca756fff69f5ab1c39ffe5f
HNMouna <hadjnasrmouna@gmail.com>
2021-05-28 13:09:31 +0100

    jsxcheckpt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Mew2 from './MEW2.jpg'
+import './Styl.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      
+     <div style={{border:'solid 1px black',maxWidth:'100vw'}}>
+
+<h1 className="title red">Your name here</h1>
+
+<br />
+
+<img src= {Mew2} />
+
+<br />
+
+<img src= './PIKA.jpg' />
+
+</div>
+
+<video width={320}  height={240} controls > 
+
+<source src="myVideo.mp4" type="video/mp4" />
+
+</video>
     </div>
   );
 }
